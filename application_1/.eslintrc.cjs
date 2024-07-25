b6e52dcc04f226643aad700d3b9f4f677a4db32b
Dmitry <dmitry.guslov-gans@yandex.ru>
2024-07-25 19:31:59 +0300

    Настроил конфигурацию linter и json проекта

--- application_1/.eslintrc.cjs
+++ application_1/.eslintrc.cjs
@@ -1,21 +1,24 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'plugin:react-hooks/recommended',
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+	settings: { react: { version: '18.2' } },
+	plugins: ['react-refresh'],
+	rules: {
+		'react/jsx-no-target-blank': 'off',
+		'react-refresh/only-export-components': [
+			'warn',
+			{ allowConstantExport: true },
+		],
+		semi: ['error', 'always', { omitLastInOneLineBlock: false }], // Указывает где я не поставил ";"
+		'comma-dangle': ['error', 'never'], // Указывает где есть висячая запятая
+		quotes: ['error', 'single'], // Все кавычки превращаем в одинарные
+	},
+};
